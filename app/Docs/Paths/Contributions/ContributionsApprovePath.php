03dcdec1d235eddf71567bc991917d7f1d17e535
Matt Inamdar <matt@ayup.agency>
2019-05-21 12:21:22 +0100

    Specified style rules

--- app/Docs/Paths/Contributions/ContributionsApprovePath.php
+++ app/Docs/Paths/Contributions/ContributionsApprovePath.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Docs\Paths\Contributions;
 
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Parameter;
 use GoldSpecDigital\ObjectOrientedOAS\Objects\PathItem;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Schema;
 
 class ContributionsApprovePath extends PathItem
 {
@@ -14,5 +16,19 @@ class ContributionsApprovePath extends PathItem
     public function __construct()
     {
         parent::__construct();
+
+        $this->route = '/contributions/{contribution}/approve';
+        $this->parameters = [
+            Parameter::path()
+                ->name('contribution')
+                ->description('The ID of the contribution')
+                ->schema(Schema::string()->format(Schema::FORMAT_UUID))
+                ->required(),
+        ];
+        $this->operations = [
+            new ShowContributionOperation(),
+            new UpdateContributionOperation(),
+            new DestroyContributionOperation(),
+        ];
     }
 }
