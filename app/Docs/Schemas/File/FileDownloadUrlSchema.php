ef6e5270d9370860399ce73f49c7702d4ad7bd07
Matt Inamdar <matt@ayup.agency>
2019-05-23 14:31:20 +0100

    Added files and file_tokens table to database ERD

--- app/Docs/Schemas/File/FileDownloadUrlSchema.php
+++ app/Docs/Schemas/File/FileDownloadUrlSchema.php
@@ -17,6 +17,8 @@ class FileDownloadUrlSchema extends Schema
         return parent::create($objectId)
             ->type(static::TYPE_OBJECT)
             ->properties(
+                Schema::string('token')
+                    ->format(Schema::FORMAT_UUID),
                 Schema::string('download_url'),
                 Schema::string('expires_at')
                     ->format(Schema::FORMAT_DATE_TIME)
