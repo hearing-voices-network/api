57fdf85cba7960fe84146a57689dd106f9820449
Matthew Inamdar <matt@goldspecdigital.com>
2019-05-29 10:53:28 +0100

    Create 2019_05_29_105029_create_files_table.php

---
+++ database/migrations/2019_05_29_105029_create_files_table.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('files', function (Blueprint $table): void {
+            $table->uuid('id')->primary();
+            $table->string('filename');
+            $table->string('mime_type');
+            $table->boolean('is_private');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('files');
+    }
+}
