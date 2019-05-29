808ff0e18d67a6f0ba554d6b4803073373893b1b
Matthew Inamdar <matt@goldspecdigital.com>
2019-05-29 10:17:25 +0100

    Created audit_actions migration

---
+++ database/migrations/2019_05_29_100642_create_audit_actions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAuditActionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('audit_actions', function (Blueprint $table): void {
+            $table->string('action')->primary();
+        });
+
+        $auditActionsPath = realpath(dirname(__DIR__)) . '/storage/audit_actions.json';
+
+        db()->table('audit_actions')->insert(
+            json_decode(file_get_contents($auditActionsPath), true)
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('audit_actions');
+    }
+}
