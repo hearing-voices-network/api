472853644afef6c386a4f0c1c05a453ea6f0a600
Matthew Inamdar <matt@goldspecdigital.com>
2019-05-29 10:31:37 +0100

    Create 2019_05_29_101747_create_audits_table.php

---
+++ database/migrations/2019_05_29_101747_create_audits_table.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAuditsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('audits', function (Blueprint $table): void {
+            $table->uuid('id')->primary();
+            $table->uuid('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedInteger('oauth_client_id')->nullable();
+            $table->foreign('oauth_client_id')->references('id')->on('oauth_clients');
+            $table->string('action');
+            $table->foreign('action')->references('action')->on('audit_actions');
+            $table->text('description')->nullable();
+            $table->string('ip_address', 45);
+            $table->text('user_agent')->nullable();
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('audits');
+    }
+}
