bfc776c9370e7a0b46b06fa8df7a0ed8989827e7
Matthew Inamdar <matt@goldspecdigital.com>
2019-05-29 15:24:56 +0100

    Encrypted end_user fields

--- app/Models/Mutators/EndUserMutators.php
+++ app/Models/Mutators/EndUserMutators.php
@@ -6,5 +6,79 @@ namespace App\Models\Mutators;
 
 trait EndUserMutators
 {
-    //
+    /**
+     * @param string|null $country
+     * @return string|null
+     */
+    public function getCountryAttribute(?string $country): ?string
+    {
+        return is_string($country) ? decrypt($country) : null;
+    }
+
+    /**
+     * @param string|null $country
+     */
+    public function setCountryAttribute(?string $country): void
+    {
+        $this->attributes['country'] = is_string($country)
+            ? encrypt($country)
+            : null;
+    }
+
+    /**
+     * @param string|null $birthYear
+     * @return string|null
+     */
+    public function getBirthYearAttribute(?string $birthYear): ?string
+    {
+        return is_string($birthYear) ? decrypt($birthYear) : null;
+    }
+
+    /**
+     * @param string|null $birthYear
+     */
+    public function setBirthYearAttribute(?string $birthYear): void
+    {
+        $this->attributes['birth_year'] = is_string($birthYear)
+            ? encrypt($birthYear)
+            : null;
+    }
+
+    /**
+     * @param string|null $gender
+     * @return string|null
+     */
+    public function getGenderAttribute(?string $gender): ?string
+    {
+        return is_string($gender) ? decrypt($gender) : null;
+    }
+
+    /**
+     * @param string|null $gender
+     */
+    public function setGenderAttribute(?string $gender): void
+    {
+        $this->attributes['gender'] = is_string($gender)
+            ? encrypt($gender)
+            : null;
+    }
+
+    /**
+     * @param string|null $ethnicity
+     * @return string|null
+     */
+    public function getEthnicityAttribute(?string $ethnicity): ?string
+    {
+        return is_string($ethnicity) ? decrypt($ethnicity) : null;
+    }
+
+    /**
+     * @param string|null $ethnicity
+     */
+    public function setEthnicityAttribute(?string $ethnicity): void
+    {
+        $this->attributes['ethnicity'] = is_string($ethnicity)
+            ? encrypt($ethnicity)
+            : null;
+    }
 }
