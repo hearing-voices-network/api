604aabd9bb7bdf24379220b450bdba29f888c5e6
Matthew Inamdar <matt@goldspecdigital.com>
2019-05-29 15:13:37 +0100

    Added getter and setter for contribution content

--- app/Models/Mutators/ContributionMutators.php
+++ app/Models/Mutators/ContributionMutators.php
@@ -6,5 +6,20 @@ namespace App\Models\Mutators;
 
 trait ContributionMutators
 {
-    //
+    /**
+     * @param string $content
+     * @return string
+     */
+    public function getContentAttribute(string $content): string
+    {
+        return decrypt($content);
+    }
+
+    /**
+     * @param string $content
+     */
+    public function setContentAttribute(string $content): void
+    {
+        $this->attributes['content'] = encrypt($content);
+    }
 }
