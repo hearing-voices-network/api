1b4651928f5b649b65a06cefd989aa2ce60bee8d
Matthew Inamdar <matt@goldspecdigital.com>
2019-05-29 10:37:33 +0100

    Added notification channels migration

---
+++ database/migrations/2019_05_29_103217_create_notification_channels_table.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotificationChannelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notification_channels', function (Blueprint $table): void {
+            $table->string('channel')->primary();
+        });
+
+        $notificationChannelsPath = realpath(dirname(__DIR__)) . '/storage/notification_channels.json';
+
+        db()->table('notification_channels')->insert(
+            json_decode(file_get_contents($notificationChannelsPath), true)
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notification_channels');
+    }
+}
