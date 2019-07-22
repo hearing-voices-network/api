0501aea0c3a2d08a9fb023f789bf9477a45ff690
Matt Inamdar <matt@ayup.agency>
2019-07-22 11:08:25 +0100

    100% test coverage for AdminService

--- tests/Unit/Services/AdminServiceTest.php
+++ tests/Unit/Services/AdminServiceTest.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace Tests\Unit\Services;
 
+use App\Models\Admin;
+use App\Models\User;
 use App\Services\AdminService;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class AdminServiceTest extends TestCase
@@ -27,10 +31,11 @@ class AdminServiceTest extends TestCase
         $this->assertEquals('John', $admin->name);
         $this->assertEquals('john.doe@example.com', $admin->user->email);
         $this->assertEquals('07700000000', $admin->phone);
+        $this->assertTrue(Hash::check('secret', $admin->user->password));
     }
 
     /** @test */
-    public function it_throws_exception_when_needed_values_are_not_provided()
+    public function it_throws_exception_when_needed_values_for_creation_are_not_provided(): void
     {
         $this->expectException(\ErrorException::class);
 
@@ -38,5 +43,56 @@ class AdminServiceTest extends TestCase
         $adminService = resolve(AdminService::class);
 
         $adminService->create([]);
+    }
+
+    /** @test */
+    public function it_updates_a_user_and_admin_record(): void
+    {
+        /** @var \App\Services\AdminService $adminService */
+        $adminService = resolve(AdminService::class);
+
+        $admin = Admin::create([
+            'name' => 'John Doe',
+            'phone' => '07700000000',
+            'user_id' => User::create([
+                'email' => 'john.doe@example.com',
+                'password' => Hash::make('secret'),
+                'email_verified_at' => Date::now(),
+            ])->id,
+        ]);
+
+        $adminService->update($admin, [
+            'name' => 'Foo Bar',
+            'phone' => '07777777777',
+            'email' => 'foo.bar@example.com',
+            'password' => 'password',
+        ]);
+
+        $this->assertEquals('Foo Bar', $admin->name);
+        $this->assertEquals('foo.bar@example.com', $admin->user->email);
+        $this->assertEquals('07777777777', $admin->phone);
+        $this->assertTrue(Hash::check('password', $admin->user->password));
+    }
+
+    /** @test */
+    public function it_deletes_a_user_and_admin_record(): void
+    {
+        /** @var \App\Services\AdminService $adminService */
+        $adminService = resolve(AdminService::class);
+
+        $admin = Admin::create([
+            'name' => 'John Doe',
+            'phone' => '07700000000',
+            'user_id' => User::create([
+                'email' => 'john.doe@example.com',
+                'password' => Hash::make('secret'),
+                'email_verified_at' => Date::now(),
+            ])->id,
+        ]);
+
+        $adminService->delete($admin);
+
+        $this->assertDatabaseMissing('admins', ['id' => $admin->id]);
+        $this->assertDatabaseMissing('users', ['id' => $admin->user_id]);
     }
 }
