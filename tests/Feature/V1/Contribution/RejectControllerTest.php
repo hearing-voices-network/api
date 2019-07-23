9dd341dafc9d353b6b38ad23db49d67354111092
Matt Inamdar <matt@ayup.agency>
2019-07-23 13:55:06 +0100

    Added audit logging for Contribution/RejectController

--- tests/Feature/V1/Contribution/RejectControllerTest.php
+++ tests/Feature/V1/Contribution/RejectControllerTest.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace Tests\Feature\V1\Contribution;
 
+use App\Events\EndpointInvoked;
 use App\Models\Admin;
+use App\Models\Audit;
 use App\Models\Contribution;
 use App\Models\EndUser;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Event;
 use Laravel\Passport\Passport;
 use Tests\TestCase;
 
@@ -113,5 +116,36 @@ class RejectControllerTest extends TestCase
         ]);
 
         $response->assertStatus(Response::HTTP_OK);
+    }
+
+    /** @test */
+    public function endpoint_invoked_event_dispatched_for_reject(): void
+    {
+        Event::fake([EndpointInvoked::class]);
+
+        $contribution = factory(Contribution::class)
+            ->state(Contribution::STATUS_IN_REVIEW)
+            ->create();
+
+        /** @var \App\Models\User $user */
+        $user = factory(Admin::class)->create()->user;
+
+        Passport::actingAs($user);
+
+        $this->putJson("/v1/contributions/{$contribution->id}/reject", [
+            'changes_requested' => 'Lorem ipsum',
+        ]);
+
+        Event::assertDispatched(
+            EndpointInvoked::class,
+            function (EndpointInvoked $event) use ($user, $contribution): bool {
+                return $event->getUser()->is($user)
+                    && $event->getClient() === null
+                    && $event->getAction() === Audit::ACTION_UPDATE
+                    && $event->getDescription() === "Rejected contribution [{$contribution->id}]."
+                    && $event->getIpAddress() === '127.0.0.1'
+                    && $event->getUserAgent() === 'Symfony';
+            }
+        );
     }
 }
