9be85aee7486bdf10a9755f4111adcba332c7c52
Matt Inamdar <matt@ayup.agency>
2019-07-23 14:12:44 +0100

    Added audit logging for File/RequestController

--- tests/Feature/V1/File/RequestControllerTest.php
+++ tests/Feature/V1/File/RequestControllerTest.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace Tests\Feature\V1\File;
 
+use App\Events\EndpointInvoked;
 use App\Models\Admin;
+use App\Models\Audit;
 use App\Models\EndUser;
 use App\Models\File;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Event;
 use Laravel\Passport\Passport;
 use Tests\TestCase;
 
@@ -71,5 +74,33 @@ class RequestControllerTest extends TestCase
             'download_url',
             'expires_at',
         ]);
+    }
+
+    /** @test */
+    public function endpoint_invoked_event_dispatched_for_request(): void
+    {
+        Event::fake([EndpointInvoked::class]);
+
+        /** @var \App\Models\File $file */
+        $file = factory(File::class)->state('private')->create();
+
+        /** @var \App\Models\User $user */
+        $user = factory(Admin::class)->create()->user;
+
+        Passport::actingAs($user);
+
+        $this->postJson("/v1/files/{$file->id}/request");
+
+        Event::assertDispatched(
+            EndpointInvoked::class,
+            function (EndpointInvoked $event) use ($user, $file): bool {
+                return $event->getUser()->is($user)
+                    && $event->getClient() === null
+                    && $event->getAction() === Audit::ACTION_CREATE
+                    && $event->getDescription() === "Requested file [{$file->id}]."
+                    && $event->getIpAddress() === '127.0.0.1'
+                    && $event->getUserAgent() === 'Symfony';
+            }
+        );
     }
 }
