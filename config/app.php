f357bc3f19d6ac22067f78e48623ffa8b6b9e61b
Matt Inamdar <matt@ayup.agency>
2019-07-29 15:35:39 +0100

    Hard coded app name in config

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Connecting Voices'),
+    'name' => 'Connecting Voices',
 
     /*
     |--------------------------------------------------------------------------
