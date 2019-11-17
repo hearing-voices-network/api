df3f7b99e5d10cd244295845d3584e2582a7f597
Matthew Inamdar <matt@goldspecdigital.com>
2019-11-17 15:27:58 +0000

    Enabled implicit grant

--- routes/passport.php
+++ routes/passport.php
@@ -21,17 +21,5 @@ Route::namespace('Laravel\\Passport\\Http\\Controllers')->group(
         Route::delete('/authorize', 'DenyAuthorizationController@deny')
             ->name('authorizations.deny')
             ->middleware(['web', 'auth:web']);
-
-        Route::post('/token', 'AccessTokenController@issueToken')
-            ->name('token')
-            ->middleware('throttle');
-
-        Route::get('/tokens', 'AuthorizedAccessTokenController@forUser')
-            ->name('tokens.index')
-            ->middleware(['web', 'auth:web']);
-
-        Route::delete('/tokens/{token_id}', 'AuthorizedAccessTokenController@destroy')
-            ->name('tokens.destroy')
-            ->middleware(['web', 'auth:web']);
     }
 );
